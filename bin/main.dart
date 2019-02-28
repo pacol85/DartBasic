dd2899446e6563af6054c4ba8ca76a69c50a85b4
Francisco Lozano <flozanog85@gmail.com>
2019-02-28 16:20:06 -0600

    for and foreach

--- bin/main.dart
+++ bin/main.dart
@@ -259,7 +259,18 @@ main(List<String> arguments) {
 }
 */
 
-//
-main(List<String> arguments) {
+//for each
 
+main(List<String> arguments) {
+  List people = ["Bryan", "Heather", "Chris"];
+
+  print(people);
+
+  for(int i = 0; i < people.length; i++){
+    print("person at ${i} is ${people[i]}");
+  }
+
+  people.forEach((person) {
+    print(person);
+  });
 }
